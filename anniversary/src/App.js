ab235d80dbf8a1f70e14cca2ffe2d167e813a4ca
Dhanushguptha19 <dhanushguptha19@gmail.com>
2022-09-27 11:02:17 +0000

    register page

---
+++ anniversary/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './header.js';
+import RegistrationForm from './registration';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <RegistrationForm/>
+    </div>
+    
+  );
+}
+export default App;
