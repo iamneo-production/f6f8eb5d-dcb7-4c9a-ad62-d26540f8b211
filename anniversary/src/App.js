c674873579eef0eee2e99e0890b7987369f23180
Dhanushguptha19 <dhanushguptha19@gmail.com>
2022-09-27 17:33:36 +0000

    reg

--- anniversary/src/App.js
+++ anniversary/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './header.js';
 import RegistrationForm from './registration';
+import signin from './signin.js';
 function App() {
   return (
     <div className="App">
@@ -12,3 +13,24 @@ function App() {
   );
 }
 export default App;
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EventDashboard from './event-dashboard';
+import EventInvitees from './event-invitees';
+import LoginForm from './auth/login-form'
+
+
+class App extends React.Component
+{
+    render()
+    {
+        return (
+            <div>
+                <LoginForm />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>,document.getElementById("app"));*/
