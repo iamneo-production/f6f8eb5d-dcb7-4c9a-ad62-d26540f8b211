28ede835125bb84d0a4d5d9c5c5c277a362793d0
NagaSanthosh123 <nagasantoshmalipeddy@gmail.com>
2022-09-27 13:41:41 +0000

    Commiting Model Classes

---
+++ src/main/java/com/veventmgmt/demo/EventMain.java
@@ -0,0 +1,10 @@
+package main.java.com.veventmgmt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class EventMain {
+    public static void main(String[]args){
+        SpringApplication.run(EventMain.class, args);
+    }
+}
