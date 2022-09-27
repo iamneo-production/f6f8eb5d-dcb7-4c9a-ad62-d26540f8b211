65bd172e684d4b9f83e7a6697a726d1657818330
NagaSanthosh123 <nagasantoshmalipeddy@gmail.com>
2022-09-27 23:30:34 +0000

    ChangesRepos

--- src/main/java/com/veventmgmt/controllers/AddOnController.java
+++ src/main/java/com/veventmgmt/controllers/AddOnController.java
@@ -1,20 +1,25 @@
 package com.veventmgmt.controllers;
 
 import com.veventmgmt.models.*;
+import com.veventmgmt.repository.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
 public class AddOnController {
+
+    @Autowired
+    private AddOnRepository addonobj;
     
     public void addAddon(AddOnModel data){
-        return;
+        addonobj.save(data);
     }
-    public void editAddOn(int addonId){
-        return;
+    public void editAddOn(int addonId,AddOnModel data){
+        addonobj.save(data);
     }
     public AddOnModel getAddOn(int addOnId){
-        return null;
+        return addonobj.findById(addOnId).get();
     }
     public void deleteAddOn(int addOnId){
-        return ;
+		addonobj.remove(addonobj.findById(addOnId).get());
     }
 
 }
