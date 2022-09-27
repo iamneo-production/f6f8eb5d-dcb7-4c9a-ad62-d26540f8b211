dca91188a3bad1d51331f6f3a818049b022c8017
NagaSanthosh123 <nagasantoshmalipeddy@gmail.com>
2022-09-27 13:50:14 +0000

    super star

--- src/main/java/com/veventmgmt/demo/EventMain.java
+++ src/main/java/com/veventmgmt/demo/EventMain.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventMain {
     public static void main(String[]args){
-        SpringApplication.run(EventMain.class, args);
-
         System.out.println("Completed");
+        SpringApplication.run(EventMain.class, args);
+        
+    
     }
 }
